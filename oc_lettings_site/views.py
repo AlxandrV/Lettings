8e3d5e1be23f9a760a40bd831f825598cea64b54
AlxandrV <a.vigneaud@gmail.com>
2022-11-07 21:31:41 +0100

    Update - remove last models before migrations

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from .models import Letting, Profile
-
-
 
 
 # Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie quam lobortis leo consectetur ullamcorper non id est. Praesent dictum, nulla eget feugiat sagittis, sem mi convallis eros,
