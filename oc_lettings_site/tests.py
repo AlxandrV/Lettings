c9ffa3ce3ae7e2a9fdcb475cb9d1dba2a731c6e8
AlxandrV <a.vigneaud@gmail.com>
2022-11-28 17:56:13 +0100

    Update - add test unit to oc_lettings_site app

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,15 @@
-def test_dummy():
-    assert 1
+from django.urls import reverse
+from django.test import TestCase
+
+from lettings.models import Address, Letting
+
+
+class TestLettings(TestCase):
+    def test_oc_lettings_site_index(self):
+        path = reverse('index')
+        res = self.client.get(path)
+
+        # Assert
+        self.assertTemplateUsed(res, 'index.html')
+        self.assertEqual(res.status_code, 200)
+        self.assertContains(res, text='<title>Holiday Homes</title>', html=True)
