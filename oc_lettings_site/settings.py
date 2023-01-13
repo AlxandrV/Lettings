b717b7103154a45b3186ed0903fc49d93f5998f2
AlxandrV <a.vigneaud@gmail.com>
2023-01-13 20:40:47 +0100

    Update - settings

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -123,11 +123,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static/"),
-]
+STATICFILES_DIRS = ()
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
 
 # Sentry porject
 sentry_sdk.init(
